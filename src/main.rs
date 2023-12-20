1c64aa0ba639e9b7bdd72d6da99895187f9c2786
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2023-12-20 15:42:16 +0100

    Stage 1: Match a literal character

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
-use std::env;
-use std::io;
-use std::process;
+use std::{env, io, process};
 
 fn match_pattern(input_line: &str, pattern: &str) -> bool {
     if pattern.chars().count() == 1 {
-        return input_line.contains(pattern);
+        input_line.contains(pattern)
     } else {
         panic!("Unhandled pattern: {}", pattern)
     }
@@ -12,11 +10,8 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
 
 // Usage: echo <input_text> | your_grep.sh -E <pattern>
 fn main() {
-    // You can use print statements as follows for debugging, they'll be visible when running tests.
-    println!("Logs from your program will appear here!");
-
     if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
+        eprintln!("Expected first argument to be '-E'");
         process::exit(1);
     }
 
@@ -25,10 +20,9 @@ fn main() {
 
     io::stdin().read_line(&mut input_line).unwrap();
 
-    // Uncomment this block to pass the first stage
-    // if match_pattern(&input_line, &pattern) {
-    //     process::exit(0)
-    // } else {
-    //     process::exit(1)
-    // }
+    if match_pattern(&input_line, &pattern) {
+        process::exit(0)
+    } else {
+        process::exit(1)
+    }
 }
