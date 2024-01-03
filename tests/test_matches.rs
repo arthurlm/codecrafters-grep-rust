affccfa33ab4fcb71e19792d47612a0bcec6f3e2
Arthur LE MOIGNE <arthur.lemoigne@gmail.com>
2024-01-03 18:13:56 +0100

    Refact tests

--- tests/test_matches.rs
+++ tests/test_matches.rs
@@ -1,147 +1,131 @@
 use grep_starter_rust::*;
 
+fn assert_match(input: &str, pattern: &str, from: usize, to: usize) {
+    assert_eq!(match_pattern(input, pattern), Some((from, to)));
+}
+
+fn assert_not_match(input: &str, pattern: &str) {
+    assert_eq!(match_pattern(input, pattern), None);
+}
+
 #[test]
 fn test_match_text() {
-    assert_eq!(match_pattern("hey ! hello world", "hello"), Some((6, 11)));
-    assert_eq!(match_pattern("Yeah", "hello"), None);
+    assert_match("hey ! hello world", "hello", 6, 11);
+    assert_not_match("Yeah", "hello");
 }
 
 #[test]
 fn test_match_digit() {
-    assert_eq!(match_pattern("hey89world", r"\d"), Some((3, 4)));
-    assert_eq!(match_pattern("Yeah", r"\d"), None);
+    assert_match("hey89world", r"\d", 3, 4);
+    assert_not_match("Yeah", r"\d");
 }
 
 #[test]
 fn test_match_chars() {
-    assert_eq!(match_pattern("alpha-num3ric", r"\w"), Some((0, 1)));
-    assert_eq!(match_pattern("foo101", r"\w"), Some((0, 1)));
-    assert_eq!(match_pattern("$!?", r"\w"), None);
+    assert_match("alpha-num3ric", r"\w", 0, 1);
+    assert_match("foo101", r"\w", 0, 1);
+    assert_not_match("$!?", r"\w");
 }
 
 #[test]
 fn test_match_pos_chars_group() {
-    assert_eq!(match_pattern("apple", r"[abc]"), Some((0, 1)));
-    assert_eq!(match_pattern("dog", r"[abc]"), None);
+    assert_match("apple", r"[abc]", 0, 1);
+    assert_not_match("dog", r"[abc]");
 }
 
 #[test]
 fn test_match_neg_chars_group() {
-    assert_eq!(match_pattern("dog", r"[^abc]"), Some((0, 1)));
-    assert_eq!(match_pattern("cab", r"[^abc]"), None);
+    assert_match("dog", r"[^abc]", 0, 1);
+    assert_not_match("cab", r"[^abc]");
 }
 
 #[test]
 fn test_match_seq() {
-    assert_eq!(match_pattern("100 apples", r"\d\d\d apple"), Some((0, 9)));
-    assert_eq!(match_pattern("1 apple", r"\d\d\d apple"), None);
-    assert_eq!(match_pattern("cab", r"\d\d\d apple"), None);
+    assert_match("100 apples", r"\d\d\d apple", 0, 9);
+    assert_not_match("1 apple", r"\d\d\d apple");
+    assert_not_match("cab", r"\d\d\d apple");
 }
 
 #[test]
 fn test_start_string() {
-    assert_eq!(match_pattern("log", "^log"), Some((0, 3)));
-    assert_eq!(match_pattern("slog", "^log"), None);
+    assert_match("log", "^log", 0, 3);
+    assert_not_match("slog", "^log");
 }
 
 #[test]
 fn test_end_string() {
-    assert_eq!(match_pattern("dog", "dog$"), Some((0, 3)));
-    assert_eq!(match_pattern("dogs", "dog$"), None);
+    assert_match("dog", "dog$", 0, 3);
+    assert_not_match("dogs", "dog$");
 }
 
 #[test]
 fn test_one_or_more() {
-    assert_eq!(match_pattern("apple", "a+"), Some((0, 1)));
-    assert_eq!(match_pattern("SaaS", "a+"), Some((1, 3)));
-    assert_eq!(match_pattern("apple", "a+pple"), Some((0, 5)));
-    assert_eq!(match_pattern("ale", "a+p+le"), None);
-    assert_eq!(match_pattern("ple", "a+p+le"), None);
-    assert_eq!(match_pattern("aple", "a+p+le"), Some((0, 4)));
-    assert_eq!(match_pattern("aaaapppppple", "a+p+le"), Some((0, 12)));
-    assert_eq!(match_pattern("aaaapppppple", "^a+p+le$"), Some((0, 12)));
-    assert_eq!(match_pattern("appLE", "a+pple"), None);
-    assert_eq!(match_pattern("SaaS", "^Sa+S$"), Some((0, 4)));
-    assert_eq!(match_pattern("dogs", "a+"), None);
-    assert_eq!(match_pattern("dogs", "d.+gs$"), Some((0, 4)));
-    assert_eq!(match_pattern(" cat and cat", r"\w+ and \w+"), Some((1, 12)));
+    assert_match("apple", "a+", 0, 1);
+    assert_match("SaaS", "a+", 1, 3);
+    assert_match("apple", "a+pple", 0, 5);
+    assert_not_match("ale", "a+p+le");
+    assert_not_match("ple", "a+p+le");
+    assert_match("aple", "a+p+le", 0, 4);
+    assert_match("aaaapppppple", "a+p+le", 0, 12);
+    assert_match("aaaapppppple", "^a+p+le$", 0, 12);
+    assert_not_match("appLE", "a+pple");
+    assert_match("SaaS", "^Sa+S$", 0, 4);
+    assert_not_match("dogs", "a+");
+    assert_match("dogs", "d.+gs$", 0, 4);
+    assert_match(" cat and cat", r"\w+ and \w+", 1, 12);
 }
 
 #[test]
 fn test_zero_or_one() {
-    assert_eq!(match_pattern("dog", "dogs?"), Some((0, 3)));
-    assert_eq!(match_pattern("dogs", "dogs?"), Some((0, 4)));
-    assert_eq!(match_pattern("dogy", "dogs?"), Some((0, 3)));
-    assert_eq!(match_pattern("dogy", "dogs?y"), Some((0, 4)));
-    assert_eq!(match_pattern("dogsy", "dogs?y"), Some((0, 5)));
-    assert_eq!(match_pattern("dogoy", "dogs?y"), None);
-    assert_eq!(match_pattern("dog", "^dogs?$"), Some((0, 3)));
-    assert_eq!(match_pattern("dogs", "^dogs?$"), Some((0, 4)));
+    assert_match("dog", "dogs?", 0, 3);
+    assert_match("dogs", "dogs?", 0, 4);
+    assert_match("dogy", "dogs?", 0, 3);
+    assert_match("dogy", "dogs?y", 0, 4);
+    assert_match("dogsy", "dogs?y", 0, 5);
+    assert_not_match("dogoy", "dogs?y");
+    assert_match("dog", "^dogs?$", 0, 3);
+    assert_match("dogs", "^dogs?$", 0, 4);
 }
 
 #[test]
 fn test_wildcard() {
-    assert_eq!(match_pattern("dog", "d.g"), Some((0, 3)));
-    assert_eq!(match_pattern("dog", "d.g"), Some((0, 3)));
-    assert_eq!(match_pattern("dg", "d.g"), None);
-    assert_eq!(match_pattern("dg", "d.?g"), Some((0, 2)));
-    assert_eq!(match_pattern("dogs", "^d.?gs?$"), Some((0, 4)));
-    assert_eq!(match_pattern("dags", "^d.?gs?$"), Some((0, 4)));
-    assert_eq!(match_pattern("dg", "^d.?gs?$"), Some((0, 2)));
-    assert_eq!(match_pattern("dig", "^d.?gs?$"), Some((0, 3)));
-    assert_eq!(match_pattern("digs", "^d.?gs?$"), Some((0, 4)));
-    assert_eq!(match_pattern("digsi", "^d.?gs?$"), None);
-    assert_eq!(match_pattern("digis", "^d.?gs?$"), None);
-    assert_eq!(match_pattern("diig", "^d.?gs?$"), None);
+    assert_match("dog", "d.g", 0, 3);
+    assert_match("dog", "d.g", 0, 3);
+    assert_not_match("dg", "d.g");
+    assert_match("dg", "d.?g", 0, 2);
+    assert_match("dogs", "^d.?gs?$", 0, 4);
+    assert_match("dags", "^d.?gs?$", 0, 4);
+    assert_match("dg", "^d.?gs?$", 0, 2);
+    assert_match("dig", "^d.?gs?$", 0, 3);
+    assert_match("digs", "^d.?gs?$", 0, 4);
+    assert_not_match("digsi", "^d.?gs?$");
+    assert_not_match("digis", "^d.?gs?$");
+    assert_not_match("diig", "^d.?gs?$");
 }
 
 #[test]
 fn test_alternation() {
-    assert_eq!(match_pattern("cat", "(cat|dog)"), Some((0, 3)));
-    assert_eq!(match_pattern("  cat  ", "(cat)"), Some((2, 5)));
-    assert_eq!(match_pattern("dog", "(cat|dog)"), Some((0, 3)));
-    assert_eq!(match_pattern("dog", "(dog)"), Some((0, 3)));
-    assert_eq!(match_pattern("dig", "(cat|dog)"), None);
-    assert_eq!(match_pattern("dog", "(cat|dog)s"), None);
-    assert_eq!(
-        match_pattern(" dogs", "(some|more)? (cat|dog)s"),
-        Some((0, 5))
-    );
-    assert_eq!(
-        match_pattern("some dogs", "(some|more) (cat|dog)s"),
-        Some((0, 9))
-    );
-    assert_eq!(match_pattern("more cat", "(some|more) (cat|dog)s"), None);
-    assert_eq!(
-        match_pattern("more cat", "(some|more) (cat|dog)s?"),
-        Some((0, 8))
-    );
-    assert_eq!(match_pattern("none dogs", "(some|more) (cat|dog)s"), None);
-    assert_eq!(
-        match_pattern(" cat and cat", r"(cat) and (cat)"),
-        Some((1, 12))
-    );
-    assert_eq!(
-        match_pattern(" cat and cat", r"(\w+) and (\w+)"),
-        Some((1, 12))
-    );
+    assert_match("cat", "(cat|dog)", 0, 3);
+    assert_match("  cat  ", "(cat)", 2, 5);
+    assert_match("dog", "(cat|dog)", 0, 3);
+    assert_match("dog", "(dog)", 0, 3);
+    assert_not_match("dig", "(cat|dog)");
+    assert_not_match("dog", "(cat|dog)s");
+    assert_match(" dogs", "(some|more)? (cat|dog)s", 0, 5);
+    assert_match("some dogs", "(some|more) (cat|dog)s", 0, 9);
+    assert_not_match("more cat", "(some|more) (cat|dog)s");
+    assert_match("more cat", "(some|more) (cat|dog)s?", 0, 8);
+    assert_not_match("none dogs", "(some|more) (cat|dog)s");
+    assert_match(" cat and cat", r"(cat) and (cat)", 1, 12);
+    assert_match(" cat and cat", r"(\w+) and (\w+)", 1, 12);
 }
 
 #[test]
 fn test_back_reference() {
-    assert_eq!(match_pattern("", r"(cat) and \1"), None);
-    assert_eq!(
-        match_pattern(" cat and cat ", r"(cat) and \1"),
-        Some((1, 12))
-    );
-    assert_eq!(match_pattern(" cat and dog ", r"(cat) and \1"), None);
-    assert_eq!(
-        match_pattern(" cat and cat ", r"(\w+) and \1"),
-        Some((1, 12))
-    );
-    assert_eq!(
-        match_pattern(" dog and dog ", r"(\w+) and \1"),
-        Some((1, 12))
-    );
-    assert_eq!(match_pattern(" cat and dog ", r"(\w+) and \1"), None);
+    assert_not_match("", r"(cat) and \1");
+    assert_match(" cat and cat ", r"(cat) and \1", 1, 12);
+    assert_not_match(" cat and dog ", r"(cat) and \1");
+    assert_match(" cat and cat ", r"(\w+) and \1", 1, 12);
+    assert_match(" dog and dog ", r"(\w+) and \1", 1, 12);
+    assert_not_match(" cat and dog ", r"(\w+) and \1");
 }
